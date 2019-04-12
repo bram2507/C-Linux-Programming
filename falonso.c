1e0bd423a382e66e572626a4b5cbb8bf7b92b603
bram2507 <abrahamalvarezgomez2507@gmail.com>
2019-04-12 02:22:56 +0200

    Se ha arrelgado la sincronizacion del carril derecho asi como la posesion de la zona central de cruce cosa que provoca choques entre los coches que provienen de arriba y los coches que provienen de la derecha.

--- falonso.c
+++ falonso.c
@@ -21,9 +21,15 @@
 #define SIGINT_SIG 10
 #define SEM_ARRAY  10
 #define SHM_SEM    1
-#define SPEED 2
+#define SPEED 60
 #define STAR_RACE 2
 #define SHM_LIMIT 300
+
+#define SEM_V 3
+#define SEM_H 4
+#define INTERCEPT_V 5
+#define INTERCEPT_H 6
+
 
 typedef struct shmemory{
             int count;
@@ -46,27 +52,31 @@ typedef struct type_message{
 int gotSIGINT = 0;
 
 void ipcrm          (int shmid, int semid, int msqid, char* buf);
-int  semaphoreLight (shMemory shm);
+int  semaphoreLight (shMemory shm,int,int);
 int  semop_PV       (int semid, int index, int numop );
 void sigintHandler  (int sn);
+int inicio          ( char **argumentos);
 
 int main(int argc, char* argv[]){
 
     int ret = 1; 
     int speed = 1; 
-    
-    if (argc == 3)
-    {
-       ret = atoi(argv[1]);
-       speed = atoi(argv[2]);
-    } else{
-        printf("Error argumentos !!!");
-        exit(EXIT_SUCCESS);
-    }
+
+            ret   = atoi(argv[1]);
+            speed = atoi(argv[2]);      
+
+    // if (argc == 3)
+    // {
+    //     if( !inicio(argv) ){
+    //     }else return (EXIT_FAILURE);
+    // } else{
+    //     printf("Error argumentos !!!");
+    //     exit(EXIT_FAILURE);
+    // }
 
     //Tratamiento de señales SIGINT y Correspondeintes
 	sigset_t sigintonly, sigall;
-	struct sigaction  actionSIGINT;
+	struct sigaction actionSIGINT;
     
 	sigfillset(&sigall);
 	sigfillset(&sigintonly);
@@ -95,23 +105,28 @@ int main(int argc, char* argv[]){
     //Reserva de Cola de Mensajes
     shm.msqid = msgget(ftok ("bin/ls",0600), IPC_CREAT | 0777);
     if ( shm.msqid == -1 ){
-        fprintf(stderr,"\n msgIdget failed \n");
+        perror("Error de creacion cola de mensajes");
         exit(EXIT_FAILURE);
     }      
     
     //Reserva de array de semaforos
     shm.semid = semget(ftok("bin/cat", 888),SEM_ARRAY,0600 | IPC_CREAT);
+    
     for (int i = 0; i < SEM_ARRAY; i++)  semctl( shm.semid, i,  SETVAL, 1); // declare 
 
-    ret = 1; //borrar por que este valor se le pasa por terminal
-    semctl( shm.semid, STAR_RACE,  SETVAL, ret);
-    semctl( shm.semid, 3,  SETVAL, 0);
+    ret = 100; //borrar por que este valor se le pasa por terminal, el uno es de la memoria
+    semctl( shm.semid, STAR_RACE, SETVAL, ret);
+    semctl( shm.semid, SEM_V, SETVAL, 1);
+    semctl( shm.semid, SEM_H, SETVAL, 1);
+    semctl( shm.semid, INTERCEPT_V, SETVAL, 0);
+    semctl( shm.semid, INTERCEPT_H, SETVAL, 0);
+    
    
     if (gotSIGINT)  ipcrm(shm.semid,shm.shmid,shm.msqid,shm.buf);
 
     //inicilizar cola de mensaje para el carril derecho  
     type_message message;
-    for ( int i = 1; i <=136; i++)
+    for ( int i = 1; i <=135; i++)
     {
       message.type= i;
       message.pos = i;
@@ -125,7 +140,7 @@ int main(int argc, char* argv[]){
     if (gotSIGINT)
     {
         ipcrm(shm.semid,shm.shmid,shm.msqid,shm.buf);
-        fprintf(stderr,"El progrma ha finalizado correctamente\n\n");
+        fprintf(stderr,"El programa ha finalizado correctamente\n\n");
         exit(EXIT_SUCCESS);
     }
     
@@ -190,13 +205,42 @@ int main(int argc, char* argv[]){
                           auxLib = 0;
                         }
                     semop_PV(shm.semid,SHM_SEM,1);
+                                     
                     
                     if (gotSIGINT)
                     {
                       shmdt((char*)shm.buf);
                       exit(0);
                     }
-                  
+
+                 
+                    if( auxLib == 105 ) {
+                         
+                        if( shm.buf[274] == ROJO || shm.buf[274] == AMARILLO){
+                           
+                            semop_PV(shm.semid, INTERCEPT_H,1);
+                            semop_PV(shm.semid, INTERCEPT_H,0);
+                            //semop_PV(shm.semid, SEM_H,1);
+                        }
+                     }
+
+                     if ( auxLib == 20){
+                           
+                          if ( shm.buf[275] == ROJO || shm.buf[275] == AMARILLO){
+                              semop_PV(shm.semid, INTERCEPT_V,1);
+                              semop_PV(shm.semid, INTERCEPT_V,0);
+                              //semop_PV(shm.semid, SEM_V,1);
+                          }
+                     }
+                           
+                    if (auxLib+1 == 21){
+                       semop_PV(shm.semid, SEM_V,-1);
+                    }else if (auxLib-4 == 21) semop_PV(shm.semid, SEM_V,1);
+                    
+                    if (auxLib+1 == 106){
+                       semop_PV(shm.semid, SEM_H,-1);
+                    }else if (auxLib-4== 106) semop_PV(shm.semid, SEM_H,1);
+
                     typeMsg = auxLib+1;
                     if ( msgrcv( shm.msqid, (void*)&message, sizeof(type_message)-sizeof(long), (int)typeMsg, 0) != -1 )
                     {
@@ -215,6 +259,13 @@ int main(int argc, char* argv[]){
                           msgsnd (shm.msqid, (struct msgbuf *)&message, 
                                                    sizeof(message.pos), 
                                                            IPC_NOWAIT);
+
+                          if (gotSIGINT)
+                          {
+                            shmdt((char*)shm.buf);
+                            exit(0);
+                          }
+
                           if (auxLib == 1)
                           {
                             message.type= 136;
@@ -242,9 +293,15 @@ int main(int argc, char* argv[]){
                     break;
         }
     }
-    
+     int    semH = VERDE;
+     int    semV = ROJO;
+    while(1)
+    {
+        if (gotSIGINT) break;
+        semaphoreLight(shm, semH, semV);
+    }
    
-    for (int i = 0; i < ret; i++) pid = wait(NULL);
+     //for (int i = 0; i < ret; i++) pid = wait(NULL);
    
     shm.count = 0; // quitar para cada vez que pase por meta  
     fin_falonso(&shm.count);
@@ -252,9 +309,23 @@ int main(int argc, char* argv[]){
     if (gotSIGINT) ipcrm(shm.semid,shm.shmid,shm.msqid,shm.buf);
     else ipcrm(shm.semid,shm.shmid,shm.msqid,shm.buf);
       
-    fprintf(stderr,"El progrma ha finalizado correctamente\n\n");
+    fprintf(stderr,"El programa ha finalizado correctamente\n\n");
 
     return EXIT_SUCCESS;
+}
+
+int inicio( char **argumentos)
+{
+    if( (atoi(argumentos[1]) < 2 || atoi(argumentos[1]) > 135)){
+        fprintf(stderr,"Primer argumento entre 2 y 135\n");
+        return 1;
+    } 
+
+    if( argumentos[2][0] != 'N' && argumentos[2][0] != 'R'){
+        fprintf(stderr,"Segundo argumento R o N\n");
+         return 1;
+    }
+return 0;
 }
 
 void ipcrm(int semid, int shmid, int msqid,char* buf){
@@ -264,34 +335,37 @@ void ipcrm(int semid, int shmid, int msqid,char* buf){
     semctl(semid,IPC_RMID,0);
 }
 
-int semaphoreLight(shMemory shm){   
-    shm.semH = VERDE; shm.semV = ROJO;
+int semaphoreLight(shMemory shm, int semH, int semV){   
+    
+    luz_semAforo(HORIZONTAL, semH);
+    luz_semAforo(VERTICAL,   semV);
+ 
+        if( shm.buf[274] == VERDE){
+           
+            // semop_PV(shm.semid, SEM_H, 1);
+            semctl( shm.semid, SEM_H, SETVAL, 1);
+            semctl( shm.semid, INTERCEPT_H, SETVAL, 0);
+           
+        }
+   
+        if( shm.buf[275] == VERDE){
+          
+            semctl( shm.semid, SEM_V, SETVAL, 1);
+            semctl( shm.semid, INTERCEPT_V, SETVAL, 0);
+           
+        }
+   
+    sleep(3);
+
+    semH++;
+    semV++;
+    if( semH ==4) semH = 1;
+ 
+    if( semV ==4) semV = 1;
 
     if (gotSIGINT) return SIGINT_SIG;
-    luz_semAforo(HORIZONTAL, shm.semH);
-    luz_semAforo(VERTICAL,   shm.semV);
-    sleep(10);//pausa();
-
-    if (gotSIGINT) return SIGINT_SIG;
-    shm.semH = AMARILLO; shm.semV = VERDE;
-    luz_semAforo(HORIZONTAL, shm.semH);
-    luz_semAforo(VERTICAL,   shm.semV);
-    sleep(10);
-
-    if (gotSIGINT) return SIGINT_SIG;
-    shm.semH = ROJO; shm.semV = AMARILLO;
-    luz_semAforo(HORIZONTAL, shm.semH);
-    luz_semAforo(VERTICAL,   shm.semV);  
-    sleep(10);
-
-    if (gotSIGINT) return SIGINT_SIG;
-    shm.semH = VERDE; shm.semV = ROJO;
-    luz_semAforo(HORIZONTAL, shm.semH);
-    luz_semAforo(VERTICAL,   shm.semV);
-    sleep(10);
-
-    if (gotSIGINT) return SIGINT_SIG;
-  
+        else semaphoreLight(shm, semH, semV);
+   
     return EXIT_SUCCESS;
 }
 
